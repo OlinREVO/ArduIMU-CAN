9ccbd04de440cfe21d7eb822ec7f1178ff239074
srli <sophia.li@students.olin.edu>
2015-01-31 15:45:17 -0500

    adding some can stuff

--- Arduimu/Arduimu.ino
+++ Arduimu/Arduimu.ino
@@ -263,6 +263,11 @@ volatile uint8_t analog_count[8];
  float Heading_X;
  float Heading_Y;
  #endif
+
+//Starting CAN
+MCP_CAN CAN(10);
+unsigned char stmp[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+
 //*****************************************************************************************
 
 void setup()
@@ -336,7 +341,17 @@ void setup()
       startup_ground();
   }
  
-  
+  if(CAN.begin(CAN_500KBPS) ==CAN_OK)  //check the Seeeduino CAN-BUS wiki for more details on code
+  {
+    Serial.print("can init ok!!\r\n");
+      for (int i = 0; i < 9; i = i + 1)
+      {
+        Serial.print(i);
+      }
+  }
+  else Serial.print("Can init fail!!\r\n");  
+
+
   delay(250);
     
   Read_adc_raw();     // ADC initialization
@@ -345,9 +360,14 @@ void setup()
   
 }
 
+
+
 //***************************************************************************************
 void loop() //Main Loop
 {
+  
+  CAN.sendMsgBuf(0x00, 0, 8, stmp);
+  delay(1000);  //
   timeNow = millis();
  
   if((timeNow-timer)>=20)  // Main loop runs at 50Hz
